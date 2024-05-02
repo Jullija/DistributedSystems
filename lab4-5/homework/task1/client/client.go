bfa0d70e4c7d9d8c1a41c0b67b2cd2cc885a2f74
Jullija <juleczek2002@gmail.com>
2024-05-02 20:40:00 +0200

    Communication works

--- lab4-5/homework/task1/client/client.go
+++ lab4-5/homework/task1/client/client.go
@@ -1,13 +1,107 @@
 package main
 
 import (
+    "bufio"
     "context"
+    "fmt"
     "log"
+    "os"
+    "strings"
     "time"
 
     "google.golang.org/grpc"
     proto "example.com/myproject/client/myproject/proto"
 )
+
+func mapLocation(input string) (proto.Location, bool) {
+    switch input {
+    case "CRACOW":
+        fmt.Print(proto.Location_CRACOW)
+        return proto.Location_CRACOW, true
+    case "LONDON":
+        return proto.Location_LONDON, true
+    case "SEDZISZOW":
+        return proto.Location_SEDZISZOW, true
+    case "ZURICH":
+        return proto.Location_ZURICH, true
+    case "LOS_ANGELES":
+        return proto.Location_LOS_ANGELES, true
+    default:
+        return 0, false
+    }
+}
+
+func getClientName() string{
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter your nickname: ")
+    name, _ := reader.ReadString('\n')
+    return name
+}
+
+
+
+func parseLocation(argument string, client proto.EventServiceClient, clientId int32, clientName string){
+    location, ok := mapLocation(argument)
+    if !ok {
+        fmt.Println("Invalid location. Please try again.")
+        return
+    }
+    fmt.Print(location)
+    request := &proto.ClientSubscribeLocationRequest{
+        ClientId:   int32(clientId),
+        ClientName: clientName,
+        Location:   location,
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    response, err := client.ClientSubscribeLocation(ctx, request)
+    if err != nil {
+        log.Printf("Error subscribing to location: %v", err)
+        return
+    }
+
+    fmt.Printf("Response: %s\n", response.Text)
+    for _, event := range response.EventsList {
+        fmt.Printf("Event ID: %d, Type: %s, Location: %s\n", event.EventId, proto.EventType_name[int32(event.Type)], proto.Location_name[int32(event.Location)])
+    }
+}
+
+func getInputs(client proto.EventServiceClient, clientId int32, clientName string) {
+    reader := bufio.NewScanner(os.Stdin)
+    for {
+        fmt.Print("<subLocation location_name>, <subType event_type>, <subId id>")
+        if reader.Scan() {
+            input := reader.Text()
+            parts := strings.Fields(input)
+            if len(parts) < 2 {
+                fmt.Println("Invalid command format. Use <subLocation location_name>, <subType event_type>, <subId id>.")
+                continue
+            }
+
+            command := parts[0]
+            argument := strings.Join(parts[1:], " ")
+
+
+            switch command{
+            case "subLocation":
+                parseLocation(argument, client, clientId, clientName)
+//             case "subType":
+//                 parseType(argument, client, clientId, clientName)
+//             case "subId":
+//                 parseId(argument, client, clientId, clientName)
+            case "quit":
+                fmt.Println("Closing connection. Thank you for listening to my TedTalk.")
+                return
+            default:
+                fmt.Println("Invalid option, please choose again. Use <subLocation location_name>, <subType event_type>, <subId id>.")
+            }
+        }
+    }
+}
+
+
 
 func main() {
     conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
@@ -16,16 +110,19 @@ func main() {
     }
     defer conn.Close()
 
-    c := proto.NewEventServiceClient(conn) // Use the 'proto' alias
-
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    c := proto.NewEventServiceClient(conn)
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // Extended deadline
     defer cancel()
 
-    req := proto.ClientConnectsRequest{ClientName: "GoClient"} // Use 'proto' for types
+    clientName := getClientName()
+    req := proto.ClientConnectsRequest{ClientName: clientName}
     r, err := c.ClientConnects(ctx, &req)
     if err != nil {
         log.Fatalf("could not connect: %v", err)
     }
     log.Printf("Client ID: %d", r.ClientId)
     log.Printf("Events: %v", r.Events)
+
+    getInputs(c, r.ClientId, clientName)
 }
+
--- lab4-5/homework/task1/server/server.py
+++ lab4-5/homework/task1/server/server.py
@@ -28,28 +28,31 @@ class EventServer(event_pb2_grpc.EventServiceServicer):
 
     def ClientSubscribeLocation(self, request, context):
         client_id = request.client_id
-        event_location = request.event_location
+        event_location = request.location
         subscribed_events = []
-        subcribed = False
+        subscribed = False
         for event in self.events:
-            if event_location == event.location and client_id not in event.attendees_ids and event.max_attendees <= len(event.attendees_ids) + 1:
-                subcribed = True
+            if event_location == event.location and client_id not in event.attendees_ids and event.max_attendees >= len(event.attendees_ids) + 1:
+                subscribed = True
                 subscribed_events.append(event)
                 event.attendees_ids.append(client_id)
 
 
-        if subcribed:
-            return event_pb2.ClientSubscribeResponse(client_id=client_id, events=subscribed_events, text=f"Subscribed for {subscribed_events}")
+
+        if subscribed:
+            return event_pb2.ClientSubscribeLocationResponse(client_id=client_id, events_list=subscribed_events, text=f"Subscribed for {subscribed_events}")
         else:
-            return event_pb2.ClientSubscribeResponse(client_id=client_id, events=[], text="Didn't subscribe for any events")
+            return event_pb2.ClientSubscribeLocationResponse(client_id=client_id, events_list=[], text="Didn't subscribe for any events")
 
 
 
 def create_events(num_events, server):
     event_type_values = [etype for etype in event_pb2.EventType.values() if etype != 0]
+    location_type_values = [loc for loc in event_pb2.Location.values() if loc != 0]
 
     for i in range(num_events):
         event_type_random = choice(event_type_values)
+        location_type_random = choice(location_type_values)
 
         event = event_pb2.Event(
             event_id=i+1,
@@ -57,7 +60,7 @@ def create_events(num_events, server):
             description="This is a random description",
             max_attendees=int(random() * 100 + 1),
             attendees_ids = [],
-            location=choice(list(event_pb2.Location.values()))
+            location=location_type_random
         )
 
         server.addEvent(event)
@@ -66,11 +69,13 @@ def create_events(num_events, server):
 def serve():
     event_server = EventServer()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    event_pb2_grpc.add_EventServiceServicer_to_server(EventServer(), server)
+    event_pb2_grpc.add_EventServiceServicer_to_server(event_server, server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Server started at port 50051")
     create_events(5, event_server)
+    print(event_server.events)
+
     server.wait_for_termination()
 
 
